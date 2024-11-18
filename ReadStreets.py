c625317872de48f0a7440ee502876d5290d72748
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2024-11-18 14:51:07 +0100

    ReadStreets will check if a tag is missing or not. Testing code...

--- ReadStreets.py
+++ ReadStreets.py
@@ -3,13 +3,15 @@ import sys
 sys.path.append('controller')
 from threading import Thread
 from Line_Tracking import *
+from RFID import *
 
 def main():
     line = Line_Tracking()
-    all_cards=[]
+    readTag = []
     card = ""
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.reset_input_buffer()
+    tagNames = RFIDTAG.__members__.keys()
     try:
         thread = Thread(target=line.run)
         thread.start()
@@ -25,11 +27,14 @@ def main():
             if ser.in_waiting > 0:
                 ser_line = ser.readline().decode('utf-8').rstrip()
                 try:
-                    new_card = ser_line.split(": ")[1]
+                    new_card = ser_line.split("Card UID: ")[1]
                     if new_card != card:
                         card = new_card
-                        if card not in all_cards:
-                            all_cards.append(card)
+                        name = RFIDTAG.name(card)
+                        if name in tagNames:
+                            del tagNames[name]
+                        if card not in readTag:
+                            readTag.append(card)
                         print(card)
                 except:
                     print("Not a card detected, probably.")
@@ -39,8 +44,9 @@ def main():
         line.IR01_GPIO.close()
         line.IR02_GPIO.close()
         line.IR03_GPIO.close()
+        print(tagNames)
         with open('all_cards.txt', 'w') as f:
-            for card in all_cards:
+            for card in readTag:
                 f.write(' '.join(card)+'\n')
         print("\nEnd of program")
 
