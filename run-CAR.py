32a8dd38302426e4919ae2178bf3192558104e13
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2025-02-05 13:03:41 +0000

    Append of folders for Python modules importing and fix of IP address

--- run-CAR.py
+++ run-CAR.py
@@ -1,10 +1,12 @@
 import sys
+sys.path.append('controller')
+sys.path.append('testbed')
 
 import comminterface
 from car_FreeNove import CarPhy
 
 if __name__ == '__main__':
-    ip = '172.16.0.10'
+    ip = '172.16.0.9'
     # ip=None
     car = CarPhy('autocloud-1', ip, 12333, 'AutoCloud1', comminterface.arg_dns_ip(sys.argv), comminterface.arg_dns_port(sys.argv))
 
