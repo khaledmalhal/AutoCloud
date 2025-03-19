4b160d7d2fbc6f5811623413c13ffcd1b4167da1
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2025-03-19 16:39:51 +0100

    Simple upload data to InfluxDB

--- controller/sensors.py
+++ controller/sensors.py
@@ -1,22 +1,51 @@
 import serial
+import os
+import influxdb_client
+from influxdb_client import Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
 
 class Sensors:
-    def __init__(self):
+    load_dotenv()
+    def __init__(self, name='nodevice'):
         self.esp32 = serial.Serial(
             '/dev/ttyUSB0', baudrate=9600, timeout=None)
-        
+        self.name = name
+        self.bucket = os.getenv("INFLUXDB_BUCKET")
+        self.token  = os.getenv("INFLUXDB_TOKEN")
+        self.org    = os.getenv("INFLUXDB_ORG")
+        self.url    = os.getenv("INFLUXDB_URL")
+        self.client = influxdb_client.InfluxDBClient(
+            url   = self.url,
+            token = self.token,
+            org   = self.org
+        )
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+
     def read_line(self):
         try:
             input = self.esp32.read_until(expected=b'\n')
-            print(input)
+            input = input.decode('utf-8').split(":")
+            key   = input[0]
+            value = input[1][:-1]
+            print(f'Key: {key}. Value: {value}.')
+
+            if key == 'Photoresistor':
+                measurement = "light"
+                field = "light"
+            elif key == 'Card UID':
+                measurement = "street"
+                field = "RFID"
+            point = Point(measurement).tag("name", self.name).field(field, value)
+            self.write_api.write(bucket=self.bucket, org=self.org, record=point)
+            return (key, value)
         except Exception as e:
             print("ERROR:{}".format(e))
 
 if __name__ == '__main__':
-    sensors = Sensors()
+    sensors = Sensors(name='autocloud-1')
     try:
         while True:
             sensors.read_line()
     except Exception as e:
         print("ERROR:{}".format(e))
-        
