1e04a2eb28330039614d88c979b8c0ba990050d3
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2025-04-22 17:10:22 +0000

    Forking light sensor and arduino sensor

--- test_influx.py
+++ test_influx.py
@@ -1,23 +1,32 @@
+import os
+
 from controller.sensors import Sensors
 from influx_api.upload import Influx
 
 from controller.ADC import Adc
 
 if __name__ == '__main__':
-    influx = Influx(name="autocloud1")
-    sensors = Sensors(name='autocloud1')
-    adc = Adc()
-    last_light = -1
-    try:
-        while True:
-            key, value = sensors.read_line()
-            influx.upload_data((key, value))
+    pid_light = os.fork()
 
-            left  = adc.readRawADS7830(0)
-            right = adc.readRawADS7830(1)
-            light = int((left + right) / 2)
-            if light != last_light:
-                last_light = light
-                influx.upload_data(('photoresistor', light))
+    try:
+        if pid_light:   # Parent. Not light.
+            print('I am parent')
+            influx = Influx(name="autocloud1")
+            sensors = Sensors(name='autocloud1')
+            while True:
+                key, value = sensors.read_line()
+                influx.upload_data((key, value))
+        else:           # Child process.
+            print('I am child')
+            influx = Influx(name="autocloud1")
+            adc = Adc()
+            last_light = -1
+            while True:
+                left  = adc.readRawADS7830(0)
+                right = adc.readRawADS7830(1)
+                light = int((left + right) / 2)
+                if light != last_light:
+                    last_light = light
+                    influx.upload_data(('photoresistor', light))
     except Exception as e:
         print("Exception:{}".format(e))
