5d3550f4f9838e4c0c7727b7a8ef2bcd9c02d201
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2025-05-20 10:17:51 +0200

    Fixed method name

--- controller/sender.py
+++ controller/sender.py
@@ -7,7 +7,7 @@ class Sender():
         self.settings = settings
         self.msg = Messages()
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.update_socket_bind()
+        self.send_sensor_data()
         print("Ready to send sensor data!")
 
     def send_sensor_data(self, data: tuple[str, str]):
